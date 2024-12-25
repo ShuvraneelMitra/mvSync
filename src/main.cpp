e305d4be64632e1eafd961ce692854db1075bfa1
Shuvraneel Mitra <mitrashuvraneel@gmail.com>
2024-12-25 23:45:08 +0530

    Linked with FFMpeg

--- src/main.cpp
+++ src/main.cpp
@@ -9,33 +9,68 @@ struct GLFWDeleter {
     }
 };
 
-int main(int argc, const char** argv){
+int main(int argc, const char** argv) {
     if (!glfwInit()) {
         std::cout << "Couldn't initialise GLFW\n";
-        return 1; 
+        return 1;
     }
 
-    std::unique_ptr<GLFWwindow, GLFWDeleter> window(glfwCreateWindow(640, 480, "Hello World", NULL, NULL));
+    std::unique_ptr<GLFWwindow, GLFWDeleter> window{glfwCreateWindow(640, 480, "Hello World", NULL, NULL)};
     if (!window) {
-        std::cout << "Could not open window\n"; 
+        std::cout << "Could not open window\n";
         return 1;
     }
 
     unsigned char* data = new unsigned char[100 * 100 * 3];
-    for(int y=0; y<100; y++){
-        for(int x=0; x<100; x++){
-            data[y * 100 * 3 + x * 3    ] = 0xff;
-            data[y * 100 * 3 + x * 3 + 1] = 0x04;
-            data[y * 100 * 3 + x * 3 + 2] = 0xf3;
+    for (int y = 0; y < 100; y++) {
+        for (int x = 0; x < 100; x++) {
+            data[y * 100 * 3 + x * 3] = 0xff;       
+            data[y * 100 * 3 + x * 3 + 1] = 0x04;  
+            data[y * 100 * 3 + x * 3 + 2] = 0xf3;  
         }
     }
 
     glfwMakeContextCurrent(window.get());
-    while(!glfwWindowShouldClose(window.get())) {
+    glEnable(GL_TEXTURE_2D);
+
+    GLuint texture_hdl;
+    glGenTextures(1, &texture_hdl);
+    glBindTexture(GL_TEXTURE_2D, texture_hdl);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 100, 100, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+    delete[] data;
+
+    while (!glfwWindowShouldClose(window.get())) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawPixels(100, 100, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+        int w, h;
+        glfwGetFramebufferSize(window.get(), &w, &h);
+        glViewport(0, 0, w, h);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, w, 0, h, -1, 1);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glBindTexture(GL_TEXTURE_2D, texture_hdl);
+        glBegin(GL_QUADS);
+        glTexCoord2d(0, 0); glVertex2i(200, 200);
+        glTexCoord2d(0, 1); glVertex2i(200, 200 + 100);
+        glTexCoord2d(1, 1); glVertex2i(200 + 100, 200 + 100);
+        glTexCoord2d(1, 0); glVertex2i(200 + 100, 200);
+        glEnd();
+
         glfwSwapBuffers(window.get());
-        glfwWaitEvents();
+        glfwPollEvents();
     }
+
+    glDeleteTextures(1, &texture_hdl);
+    glfwTerminate();
     return 0;
 }
